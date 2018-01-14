127ae2cc6bcd8922496ab59bf9a0652c861fa4c4
Rob <Rob@DELL>
2018-01-14 11:00:02 -0600

    edited for formatting

--- PhoneConvert2/src/phoneConvert/Phone.java
+++ PhoneConvert2/src/phoneConvert/Phone.java
@@ -5,54 +5,45 @@ import java.util.Scanner;
 public class Phone
 {
 
-    public static void main(String[] args)
-    {
-        String workChar = null;
-        String phoneAlpha = "STATEFARM";
-        String phoneNumeric = "";
-        String[] digits = new String[]
-        { "ABC2", "DEF3", "GHI4", "JKL5", "MNO6", "PQRS7", "TUV8", "WXYZ9" };
-        System.out.println("Enter you alpha number: ");
-        // Scanner input = new Scanner(System.in);
-        // phoneAlpha = input.next();
-        phoneAlpha = phoneAlpha.toUpperCase();
+   public static void main(String[] args)
+   {
+      String workChar = null;
+      String phoneAlpha = "STATEFARM";
+      String phoneNumeric = "";
+      String[] digits = new String[] { "1", "ABC2", "DEF3", "GHI4", "JKL5", "MNO6", "PQRS7", "TUV8", "WXYZ9", "0" };
 
-        if (phoneAlpha.length() != 7)
-        {
-            phoneAlpha = phoneAlpha.substring(0, 7);
-        }
+      System.out.print("Enter you alpha number: ");
+      Scanner input = new Scanner(System.in);
+      
+      phoneAlpha = input.next();
+      phoneAlpha = phoneAlpha.toUpperCase();
 
-        for (int i = 0; i < phoneAlpha.length(); i++)
-        {
-            workChar = phoneAlpha.substring(i, i + 1);
-            // System.out.println("working character:");
-            // System.out.println(workChar);
-            int found = 0;
-            for (int j = 0; j < digits.length; j++)
+      if (phoneAlpha.length() != 7)
+      {
+         phoneAlpha = phoneAlpha.substring(0, 7);
+      }
+
+      for (int i = 0; i < phoneAlpha.length(); i++)
+      {
+         workChar = phoneAlpha.substring(i, i + 1);
+
+         for (int j = 0; j < digits.length; j++)
+         {
+
+            if (digits[j].contains(workChar))
             {
-                // System.out.println(j);
-                found = 0;
-                if (digits[j].contains(workChar))
-                {
-                    phoneNumeric = phoneNumeric
-                            + digits[j].substring(digits[j].length() - 1);
-                    found = 1;
-                }
-
-            }
-            // if (found == 0) {
-            // phoneNum = phoneNum + workChar;
-            // found=0;
-            // }
-            if (i == 2)
-            {
-                phoneNumeric = phoneNumeric + "-";
+               phoneNumeric = phoneNumeric + digits[j].substring(digits[j].length() - 1);
             }
 
+         }
+         if (i == 2)
+         {
+            phoneNumeric = phoneNumeric + "-";
+         }
+         input.close();
+      }
+      System.out.println("Your converted phone number is: " + phoneNumeric);
 
-        }
-        System.out.println("Your converted phone number is: " + phoneNumeric);
-
-    }
+   }
 
 }
